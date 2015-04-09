1685d2c8a68674b60be850ad44c29669f9e24724
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2015-04-09 08:49:27 +0200

    added types.jl

---
+++ src/types.jl
@@ -0,0 +1,15 @@
+## Iextractor is a type that contains the information necessary for i-vector extraction:
+## The T-matrix and an updated precision matrix prec
+## It is difficult to decide how to store T and Σ, as T' and vec(prec)?
+type IExtractor{T<:FloatingPoint}
+    Tt::Matrix{T}
+    prec::Vector{T}
+    function IExtractor(Tee::Matrix, prec::Vector)
+        @assert size(Tee,1) == length(prec)
+        new(Tee', prec)
+    end
+end
+IExtractor{T<:FloatingPoint}(Tee::Matrix{T}, prec::Vector{T}) = IExtractor{T}(Tee, prec)
+## or initialize with a traditional covariance matrix
+IExtractor{T<:FloatingPoint}(Tee::Matrix{T}, Σ::Matrix{T}) = IExtractor{T}(Tee, vec(1./Σ'))
+
