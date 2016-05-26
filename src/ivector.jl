8f17eb2d9509900a098d7304babfaa5f41c1cc7f
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2016-05-26 17:41:12 +0200

    Change everything to shifted and scaled stats

--- src/ivector.jl
+++ src/ivector.jl
@@ -13,135 +13,87 @@ function Iplusx!{T}(x::Matrix{T})
     return x
 end
 
+## this constructs the T matrix from the IExtractor vector-of-component-T's
+Tᵀ(ie::IExtractor) = hcat([Tc' for Tc in ie.T]...)
+
 ## compute variance and mean of the posterior distribution of the hidden variables
 ## s: centered statistics (.N .F .S),
 ## v: svl x Nvoices matrix, initially random
 ## Σ: ng x nfea supervector diagonal covariance matrix, intially gmm.Σ
 ## result is a Nvoices vector and Nvoices x Nvoices matrix
-function posterior{T<:AbstractFloat}(s::Cstats{T}, v::Matrix{T}, Σ::Matrix{T})
-    svl, nv = size(v)
-    @assert prod(size(s.F)) == prod(size(Σ)) == svl
-    NΛ = svec(broadcast(/, s.N, Σ))  # svl; use correct order in super vector
-    ## cov = inv(I + v' * broadcast(*, NΛ, v)) # inv(l), a nv * nv matrix; Nmul: svl * nv + svl * nv^2
-    vᵀNΛv = v' * broadcast(*, NΛ, v)
-    cov = inv(Iplusx!(vᵀNΛv))
-    FΛ =  svec(s.F ./ Σ)                # svl
-    μ = cov * (v' * FΛ)                 # Nmul: svl * nv + nv^2
-    μ, cov                              # nv and nv * nv
-end
-
-## compute expectations E[y] and E[y yᵀ]
-function expectation(s::Cstats, v::Matrix, Σ::Matrix)
-    Ey, cov = posterior(s, v, Σ)
-    Eyyᵀ = Ey * Ey' + cov
-    Ey, Eyyᵀ
+function posterior{T<:AbstractFloat}(ie::IExtractor{T}, s::CSstats{T})
+    nvoices = size(ie.T[1], 2)
+    cov = eye(T, nvoices)
+    for (n, TTᵀ) in zip(s.n, ie.TTᵀ)
+        Base.LinAlg.BLAS.axpy!(n, TTᵀ, cov)
+    end
+    Linv = inv(cov)
+    w = Linv * (Tᵀ(ie) * svec(s.f))                 # Nmul: svl * nv + nv^2
+    return w, Linv + w * w'                             # nv and nv * nv
 end
 
 ## same for an array of stats
-function expectation{T1,T2}(s::Vector{Cstats{T1,T2}}, v::Matrix, Σ::Matrix)
-    pmap(x->expectation(x, v,  Σ), s)
-end
-
-## As BLAS saxpy!
-function axpy!(c::Matrix, a, b::Matrix)
-    length(c) == length(b) || throw(DimensionMismatch("Input and output sizes must match"))
-    for i in 1:length(c)
-        c[i] += a * b[i]
-    end
+function posterior{T}(ie::IExtractor, s::Vector{CSstats{T}})
+    pmap(x->posterior(ie, x), s)
 end
 
 ## update v and Σ according to the maximum likelihood re-estimation,
 ## S: vector of Cstats (0th, 1st, 2nd order stats)
 ## ex: vectopr of expectations, i.e., tuples E[y], E[y y']
 ## v: projection matrix
-function updatevΣ!{T<:AbstractFloat,T2}(S::Vector{Cstats{T,T2}}, ex::Vector, v::Matrix; updateΣ=false)
-    @assert length(S) == length(ex)
-    ng, nfea = size(first(S).F)     # number of components or Gaussians
-    svl = ng*nfea                   # supervector lenght, CF
-    nv = length(first(ex)[1])       # number of voices
-    N = zeros(ng)
-    A = map(x->zeros(nv,nv), 1:ng)
-    C = zeros(svl, nv)
-    for (s,e) in zip(S, ex)         # loop over all utterances
-        Ey, Eyyᵀ = e
-        n = s.N
-        N += n
-        for c=1:ng
-            ## axpy!(A[c], n[c], Σ)
-            Base.LinAlg.BLAS.axpy!(n[c], Eyyᵀ, A[c]) # Eyyᵀ
+function updateie!{T<:AbstractFloat}(ie::IExtractor, S::Vector{CSstats{T}}, post::Vector)
+    @assert length(S) == length(post)
+    ng = length(ie.T)
+    nfea, nv = size(ie.T[1])
+    A = map(x -> zeros(nv, nv), 1:ng)
+    C = zeros(ng * nfea, nv)
+    for (s, p) in zip(S, post)         # loop over all utterances
+        Ey, Eyyᵀ = p
+        for c in 1:ng
+            Base.LinAlg.BLAS.axpy!(s.n[c], Eyyᵀ, A[c]) # Eyyᵀ
         end
         # C += svec(s.F) * μ'          # Ey
-        Base.LinAlg.BLAS.gemm!('N', 'T', 1.0, svec(s.F), Ey, 1.0, C)
+        Base.LinAlg.BLAS.gemm!('N', 'T', 1.0, svec(s.f), Ey, 1.0, C)
     end
-    ## update v
-    ## v = Array(T, svl, nv)
     for c=1:ng
         range = ((c-1)*nfea+1) : c*nfea
-        v[range,:] = C[range,:] / A[c] ## C[range,:] * inv(A[c])
-    end
-    if !updateΣ
-        return
-    else
-        ## update Σ
-        Σ = -reshape(sum(C .* v, 2), nfea, ng)'    # diag(C * v')
-        for s in S
-            ## Σ += s.S
-            broadcast!(+, Σ, Σ, s.S)
-        end
-        broadcast!(/, Σ, Σ, N)
-        return Σ
+        ie.T[c][:] = C[range,:] / A[c] ## C[range,:] * inv(A[c])
+        Base.BLAS.gemm!('T', 'N', 1.0, ie.T[c], ie.T[c], 0.0, ie.TTᵀ[c])  ## TTᵀ[c] = T[c]' * T
     end
 end
 
 import GaussianMixtures.em!
 
-function em!{T1,T2}(ie::IExtractor{T1}, S::Vector{Cstats{T1,T2}}; nIter=1, updateΣ=false)
+function em!{T1,T2}(ie::IExtractor{T1}, S::Vector{Cstats{T1,T2}}; nIter=1)
     v = ie.T
     ng = length(S[1].N)
     nfea = length(ie.Λ) ÷ ng
     Σ = reshape(ie.Λ, nfea, ng)'
     for i=1:nIter
         print("Iteration ", i, "...")
-        ex = expectation(S, v, Σ)
-        if updateΣ
-            Σ = updatevΣ!(S, ex, v, updateΣ=true)
-        else
-            updatevΣ!(S, ex, v)
-        end
+        ex = expectation(S, v)
+        updatevΣ!(S, ex, v)
         println("done")
     end
     return ie
 end
 
 ## Train an ivector extractor matrix
-function IExtractor{T1<:AbstractFloat,T2}(ubm::GMM, S::Vector{Cstats{T1,T2}}, nvoices::Int; nIter=7, updateΣ=false)
-    ng, nfea = size(first(S).F)
-    v = randn(ng*nfea, nvoices) * sum(ubm.Σ) * 0.001
-    Σ = ubm.Σ
+function IExtractor{T1<:AbstractFloat}(S::Vector{CSstats{T1}}, nvoices::Int; nIter=7)
+    ng, nfea = size(first(S).f)
+    T = map(i -> randn(nfea, nvoices), 1:ng)
+    ie = IExtractor(T)
     for i=1:nIter
         print("Iteration ", i, "...")
-        ex = expectation(S, v, Σ)
-        if updateΣ
-            Σ = updatevΣ!(S, ex, v, updateΣ=true)
-        else
-            updatevΣ!(S, ex, v)
-        end
+        post = posterior(ie, S)
+        updateie!(ie, S, post)
         println("done")
     end
-    IExtractor(v, Σ)
+    return ie
 end
-## backwards compatibility
-IExtractor{T1<:AbstractFloat,T2}(S::Vector{Cstats{T1,T2}}, ubm::GMM, nvoices::Int; nIter=7) = IExtractor(ubm, S, nvoices)
 
 # extract an ivector using T-matrix and uncentered stats
-function ivector(ie::IExtractor, s::Cstats)
-    nv = size(ie.T, 2)
-    ng = length(s.N)
-    nfea = length(ie.Λ) ÷ ng
-    TᵀΣF = ie.T' * (svec(s.F) .* ie.Λ)
-    NΛ = vec(broadcast(*, s.N', reshape(ie.Λ, nfea, ng))) # Kenny-order
-    ## w = inv(eye(nv) + ie.Tᵀ * broadcast(*, NΛ, ie.Tᵀ')) * TᵀΣF
-    TᵀNΛT = ie.T' * broadcast(*, NΛ, ie.T)
-    w = Iplusx!(TᵀNΛT) \ TᵀΣF
+function ivector(ie::IExtractor, s::CSstats)
+    w, = posterior(ie, s)
     return w
 end
--- src/types.jl
+++ src/types.jl
@@ -1,28 +1,12 @@
 ## Iextractor is a type that contains the information necessary for i-vector extraction:
-## The T-matrix and an updated precision matrix prec
-## It is difficult to decide how to store T and Σ, as T' and vec(Λ)?
-type IExtractor{T<:AbstractFloat}
-    T::Matrix{T}
-    Λ::Vector{T} ## Kenny-order precision, "gaussian index"-major
-    function IExtractor(Tee::Matrix, Λ::Vector)
-        size(Tee,1) == length(Λ) || throw(DimensionMismatch("nrows(T) != length(Λ)"))
-        new(Tee, Λ)
+## We now stor T as a vector of Nfea x Nvoices of length Ngaussians
+## we reserve room for pre-multiplied TTᵀ
+type IExtractor{F<:AbstractFloat}
+    T::Vector{Matrix{F}}
+    TTᵀ::Vector{Matrix{F}}
+    function IExtractor(T::Vector)
+        TTᵀ = map(x -> x' * x, T)
+        new(T, TTᵀ)
     end
 end
-IExtractor{T<:AbstractFloat}(Tee::Matrix{T}, prec::Vector{T}) = IExtractor{T}(Tee, prec)
-## or initialize with a traditional covariance matrix
-IExtractor{T<:AbstractFloat}(Tee::Matrix{T}, Σ::Matrix{T}) = IExtractor{T}(Tee, vec(1./Σ'))
-
-## backward compatibility
-type oldIExtractor{T<:AbstractFloat}
-    Tᵀ::Matrix{T}
-    Λ::Vector{T} ## Kenny-order precision, "gaussian index"-major
-    function IExtractor(Tee::Matrix, Λ::Vector)
-        size(Tee,1) == length(Λ) || throw(DimensionMismatch("nrows(T) != length(Λ)"))
-        new{typeof(Tee)}(Tee', Λ)
-    end
-end
-
-import JLD, JLD.translate
-JLD.readas(x::oldIExtractor) = IExtractor(x.Tᵀ', x.Λ)
-JLD.translate("oldIExtractor", "IExtractor")
+IExtractor{F}(T::Vector{Matrix{F}}) = IExtractor{F}(T)
