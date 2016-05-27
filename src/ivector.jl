412ec3558e90f76fb1a628c8c5ccfeabfced628c
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2016-05-27 09:29:40 +0200

    Refactor TTᵀ to TᵀT, and add gemm!-based multiple ivector-extractor

--- src/ivector.jl
+++ src/ivector.jl
@@ -1,6 +1,13 @@
 ## ivector.jl  Various routines for ivector extraction
 ## (c) 2013--2014 David A. van Leeuwen
 
+## a global for timing alternative code...
+alt=false
+
+function setalt(a)
+    global alt=a
+end
+
 ## Kenny-order supervectors correspond to GMM-order matrices by stacking row-wise, i.e., svec(X) = vec(X')
 ## for g in Gaussians for f in features X[g,f] end end
 svec(x::Matrix) = vec(x')
@@ -13,23 +20,34 @@ function Iplusx!{T}(x::Matrix{T})
     return x
 end
 
-## this constructs the T matrix from the IExtractor vector-of-component-T's
+## This constructs the T matrix from the IExtractor vector-of-component-T's
+## This is actually inefficient, because it declares a lot of memory
 Tᵀ(ie::IExtractor) = hcat([Tc' for Tc in ie.T]...)
 
 ## compute variance and mean of the posterior distribution of the hidden variables
-## s: centered statistics (.N .F .S),
-## v: svl x Nvoices matrix, initially random
-## Σ: ng x nfea supervector diagonal covariance matrix, intially gmm.Σ
+## s: centered and scaled statistics (.n .f),
+## ie.T: ng x (nfea x nvoices) matrix
 ## result is a Nvoices vector and Nvoices x Nvoices matrix
-function posterior{T<:AbstractFloat}(ie::IExtractor{T}, s::CSstats{T})
+function posterior{Float<:AbstractFloat}(ie::IExtractor{Float}, s::CSstats{Float}; Linv=true)
     nvoices = size(ie.T[1], 2)
-    cov = eye(T, nvoices)
-    for (n, TTᵀ) in zip(s.n, ie.TTᵀ)
-        Base.LinAlg.BLAS.axpy!(n, TTᵀ, cov)
+    cov = eye(Float, nvoices)
+    for (n, TᵀT) in zip(s.n, ie.TᵀT)
+        Base.BLAS.axpy!(n, TᵀT, cov)
     end
-    Linv = inv(cov)
-    w = Linv * (Tᵀ(ie) * svec(s.f))                 # Nmul: svl * nv + nv^2
-    return w, Linv + w * w'                             # nv and nv * nv
+    ## Tᵀf = Tᵀ(ie) * svec(s.f)
+    Tᵀf = zeros(Float, nvoices)
+    for (c, Tc) in enumerate(ie.T)
+        Base.BLAS.gemm!('T', 'T', 1.0, Tc, sub(s.f, c, :), 1.0, Tᵀf)
+    end
+    if Linv
+        L⁻¹ = inv(cov)
+        w = L⁻¹ * Tᵀf
+        Base.BLAS.gemm!('N', 'T', 1.0, w, w, 1.0, L⁻¹) ## L⁻¹ += w * w'
+        return w, L⁻¹
+    else
+        w = cov \ Tᵀf
+        return w
+    end
 end
 
 ## same for an array of stats
@@ -52,27 +70,23 @@ function updateie!{T<:AbstractFloat}(ie::IExtractor, S::Vector{CSstats{T}}, post
         for c in 1:ng
             Base.LinAlg.BLAS.axpy!(s.n[c], Eyyᵀ, A[c]) # Eyyᵀ
         end
-        # C += svec(s.F) * μ'          # Ey
+        # C += svec(s.F) * Ey
         Base.LinAlg.BLAS.gemm!('N', 'T', 1.0, svec(s.f), Ey, 1.0, C)
     end
     for c=1:ng
         range = ((c-1)*nfea+1) : c*nfea
         ie.T[c][:] = C[range,:] / A[c] ## C[range,:] * inv(A[c])
-        Base.BLAS.gemm!('T', 'N', 1.0, ie.T[c], ie.T[c], 0.0, ie.TTᵀ[c])  ## TTᵀ[c] = T[c]' * T
+        Base.BLAS.gemm!('T', 'N', 1.0, ie.T[c], ie.T[c], 0.0, ie.TᵀT[c])  ## TTᵀ[c] = T[c]' * T
     end
 end
 
 import GaussianMixtures.em!
 
-function em!{T1,T2}(ie::IExtractor{T1}, S::Vector{Cstats{T1,T2}}; nIter=1)
-    v = ie.T
-    ng = length(S[1].N)
-    nfea = length(ie.Λ) ÷ ng
-    Σ = reshape(ie.Λ, nfea, ng)'
+function em!{T1}(ie::IExtractor{T1}, S::Vector{CSstats{T1}}; nIter=1)
     for i=1:nIter
         print("Iteration ", i, "...")
-        ex = expectation(S, v)
-        updatevΣ!(S, ex, v)
+        post = posterior(ie, S)
+        updateie!(ie, S, post)
         println("done")
     end
     return ie
@@ -92,8 +106,27 @@ function IExtractor{T1<:AbstractFloat}(S::Vector{CSstats{T1}}, nvoices::Int; nIt
     return ie
 end
 
-# extract an ivector using T-matrix and uncentered stats
+## extract an ivector using T-matrix and centered stats
 function ivector(ie::IExtractor, s::CSstats)
-    w, = posterior(ie, s)
+    w = posterior(ie, s, Linv=false)
     return w
+end
+
+## extract multiple ivectors efficiently
+function ivector{Float}(ie::IExtractor{Float}, S::Vector{CSstats{Float}})
+    nvoices = size(ie.T[1], 2)
+    nfea, nutt = size(S[1].f, 2), length(S)
+    covs = repmat(vec(eye(Float, nvoices)), 1, length(S)) ## nvoices^2 x nutt
+    n = hcat([s.n for s in S]...) ## ng x nutt
+    for (i, TᵀT) in enumerate(ie.TᵀT)
+        Base.BLAS.gemm!('N', 'N', 1.0, vec(TᵀT), sub(n, i, :), 1.0, covs) # nvoices^2 x 1, 1 x nutt, nvoices^2 x nutt
+    end
+    ## Tᵀf = Tᵀ(ie) * svec(s.f)
+    Tᵀfs = zeros(Float, nvoices, nutt) ## nvoices x nutt
+    f = cat(3, [s.f for s in S]...)
+    for (c, Tc) in enumerate(ie.T)
+        fc = reshape(f[c, :, :], nfea, nutt)
+        Base.BLAS.gemm!('T', 'N', 1.0, Tc, fc, 1.0, Tᵀfs) # nfea x nvoices, nfea x nutt, nvoices x nutt
+    end
+    map(i -> reshape(covs[:,i], nvoices, nvoices) \ reshape(Tᵀfs[:,i], nvoices), 1:nutt)
 end
--- src/types.jl
+++ src/types.jl
@@ -1,12 +1,12 @@
 ## Iextractor is a type that contains the information necessary for i-vector extraction:
 ## We now stor T as a vector of Nfea x Nvoices of length Ngaussians
-## we reserve room for pre-multiplied TTᵀ
+## we reserve room for pre-multiplied TᵀT
 type IExtractor{F<:AbstractFloat}
     T::Vector{Matrix{F}}
-    TTᵀ::Vector{Matrix{F}}
+    TᵀT::Vector{Matrix{F}}
     function IExtractor(T::Vector)
-        TTᵀ = map(x -> x' * x, T)
-        new(T, TTᵀ)
+        TᵀT = map(x -> x' * x, T)
+        new(T, TᵀT)
     end
 end
 IExtractor{F}(T::Vector{Matrix{F}}) = IExtractor{F}(T)
